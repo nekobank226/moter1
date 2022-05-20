415c19a2ddf20a50de445d6d1880e144f1304f8e
nekobank <103983489+nekobank226@users.noreply.github.com>
2022-05-20 16:15:13 +0900

    完成

--- moter.py
+++ moter.py
@@ -17,31 +17,35 @@ PCA9685.set_pwm_freq(SET_FREQ)
 #モータの回転速度を引数にする関数
 def moterspeed(v):
     #時計回り
-    if(v>0,v<100):
+    if(v>0 and v<=100):
+        print("回転開始")
         pi.write(14,1)
         pi.write(15,0)
-        PCA9685.set_pwm(0,0,v*0.01*4095)
+        a= v*40
+        int(a)
+        PCA9685.set_pwm(0,0,a)
 
-    else(v<0,v>-100):
+    #半時計周り
+    elif(v<0 and v>-100):
+        print("逆回転開始")
         pi.write(14,0)
         pi.write(15,1)
-        v = -v
-        PCA9685.set_pwm(0,0,v*0.01*4095)
+        v = -1*v
+        a =v*40
+        int(a)
+        PCA9685.set_pwm(0,0,a)
 
-    else(v>100,v<-100):
+    #vの値が大きい時
+    elif(v>100 and v<-100):
         pi.write(14,0)
         pi.write(15,0)
         PCA9685.set_pwm(0,0,0)
 
-v = 50
+moterspeed(50)
 time.sleep(3)
-v = -50
+moterspeed(-50)
 time.sleep(3)
-v = 1
+moterspeed(25)
 time.sleep(3)
-#停止
-pi.write(14,0)
-pi.write(15,0)
+print("終了")
 PCA9685.set_pwm(0,0,0)
-
-
